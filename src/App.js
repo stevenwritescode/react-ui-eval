a10a37bb70c402004a55fbc82ebc0a8687a6cb14
Eric Turner <eric.turner@7factor.io>
2019-06-05 10:51:21 -0400

    Removing things

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
-import Grid from './grid';
 
 function App() {
   return (
     <div className="App">
       Code goes here!
-      <Grid></Grid>
     </div>
   );
 }
